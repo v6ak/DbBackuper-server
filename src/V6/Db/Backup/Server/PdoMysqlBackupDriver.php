f0a37e204a9e56a170ad6530ac9d63ae53e26062
Vít Šesták <vit6ak@gmail.com>
2010-02-05 15:03:57 +0100

    unknown

--- src/V6/Db/Backup/Server/PdoMysqlBackupDriver.php
+++ src/V6/Db/Backup/Server/PdoMysqlBackupDriver.php
@@ -4,14 +4,27 @@ require_once dirname(__FILE__).'/BackupDriver.php';
 
 final class V6_Db_Backup_Server_PdoMysqlBackupDriver implements V6_Db_Backup_Server_BackupDriver{
 	
+	private $pdb;
+	
+	public function __construct($dbh){
+		if(! ($dbh instanceof PDO) ){
+			throw new InvalidArgumentException('$dbh has to be a PDO');
+		}
+		$this->dbh = $dbh;
+	}
+	
 	public function listTables(){
-		return $dbh->query('SHOW TABLES')->fetchAll(PDO::FETCH_COLUMN);
+		return $this->dbh->query('SHOW TABLES')->fetchAll(PDO::FETCH_COLUMN);
+	}
+	
+	public function quote($value){
+		return $this->dbh->quote($value);
 	}
 	
 	// FIXME: implement
-	public function quote($value){}
-	
-	public function getTableData($name){}
+	public function getTableData($name){
+		return $this->dbh->query('SELECT * FROM '.$name);
+	}
 	
 	public function getTableDefinition($name){}
 	

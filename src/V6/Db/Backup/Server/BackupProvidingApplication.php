57bdff9c20751df2fe8b57af89ed552b5b93a4c9
Vít Šesták <vit6ak@gmail.com>
2010-02-05 14:28:54 +0100

    + constructor assignment

--- src/V6/Db/Backup/Server/BackupProvidingApplication.php
+++ src/V6/Db/Backup/Server/BackupProvidingApplication.php
@@ -14,6 +14,7 @@ final class V6_Db_Backup_Server_BackupProvidingApplication{
 		if(! ($backupDriver instanceof V6_Db_Backup_Server_BackupProvidingApplication_BackupDriver) ){
 			throw new InvalidArgumentException('$backupDriver has to be a V6_Db_Backup_Server_BackupProvidingApplication_BackupDriver');
 		}
+		$this->backupDriver = $backupDriver;
 	}
 	
 	public function run(){
